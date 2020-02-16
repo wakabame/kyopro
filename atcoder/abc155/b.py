3662d78595c3f83279d291ecd617152ec61cbb15
Yuki Kimura <wakabame89@gmail.com>
2020-02-17 00:11:19 +0900

    Add ABC155

---
+++ atcoder/abc155/b.py
@@ -0,0 +1,18 @@
+N = int(input())
+A = list(map(int, input().split()))
+
+flag = 1
+for i in range(N):
+    if A[i] %2 == 1:
+        continue
+    elif A[i]%3 == 0 :
+        continue
+    elif A[i]%5== 0:
+        continue
+    else:
+        flag = 0
+
+if flag :
+    print("APPROVED")
+else:
+    print("DENIED")
---
+++ atcoder/abc155/e.py
@@ -0,0 +1,31 @@
+def solve_dp(s):
+    n = len(s)
+    s = [int(c) for c in s]
+    inf = 10**9
+
+    dp = [[inf] * 2 for _ in range(n)]
+
+    d0 = s[0]
+    dp[0][0] = d0
+    dp[0][1] = d0+1
+
+    for i in range(1, n):
+        d = s[i]
+        for k in range(10):
+            if d + k >= 10:
+                dp[i][0] = min(dp[i][0], dp[i-1][1] + 2*k + d - 10)
+            else:
+                dp[i][0] = min(dp[i][0], dp[i-1][0] + 2*k + d)
+        d += 1
+        for k in range(10):
+            if d + k >= 10:
+                dp[i][1] = min(dp[i][1], dp[i-1][1] + 2*k + d - 10)
+            else:
+                dp[i][1] = min(dp[i][1], dp[i-1][0] + 2*k + d)
+
+    return dp[n-1][0]
+
+if __name__ == '__main__':
+    s = input()
+    s = "0"+ s
+    print(solve_dp(s))
