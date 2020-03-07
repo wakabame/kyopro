326b4f8f615f78020eb4e6377835e5c7e5a15613
Yuki Kimura <wakabame89@gmail.com>
2020-03-08 02:26:51 +0900

    Modify abc156-d

--- atcoder/abc156/d.py
+++ atcoder/abc156/d.py
@@ -1,11 +1,17 @@
-from functools import reduce
+def perm(n, k, p):
+  ret = 1
+  for i in range(n, n-k, -1):
+    ret = (ret * i)%p
+  return ret
 
 def comb(n, k, p):
-  a = reduce(lambda x,y: x*y%p, range(n,n-k,-1))
-  b = reduce(lambda x,y: x*y%p, range(1,k+1))
+  """power_funcを用いて(nCk) mod p を求める"""
+  a = perm(n, k, p)
+  b = perm(k, k, p)
   return (a*pow(b, p-2, p))%p
-      
+
 n, a, b = map(int, input().split())
+
 MOD = 10**9 + 7
-     
+
 print((pow(2,n,MOD) - 1 - comb(n, a, MOD) - comb(n, b, MOD))%MOD)
