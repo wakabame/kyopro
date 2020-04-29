cc589979ff3f8aec26aba24127d6acf7935b2d51
Yuki Kimura <wakabame89@gmail.com>
2020-04-29 23:58:14 +0900

    Add ABC159 D

---
+++ atcoder/abc159/d.py
@@ -0,0 +1,11 @@
+N = int(input())
+A = list(map(int, input().split()))
+
+Li = [0] * max(A)
+for a in A:
+    Li[a-1] += 1
+
+ans = sum(a*(a-1)//2 for a in Li)
+
+for a in A:
+    print(ans - (Li[a-1]-1))
