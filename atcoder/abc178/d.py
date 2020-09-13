b9394966930dfcfd7fe46bc7f7cec2cf1c2598df
Yuki Kimura <wakabame89@gmail.com>
2020-09-14 00:07:56 +0900

    Add abc178

---
+++ atcoder/abc178/d.py
@@ -0,0 +1,22 @@
+def perm(n, k, p):
+  ret = 1
+  for i in range(n, n-k, -1):
+    ret = (ret * i)%p
+  return ret
+
+def comb(n, k, p):
+  a = perm(n, k, p)
+  b = perm(k, k, p)
+  return (a*pow(b, p-2, p))%p
+
+S = int(input())
+
+ans = 0
+S -= 3
+t = 0
+while S >= 0:
+    ans += comb(S+t,t, 10**9+7)
+    S -= 3
+    t += 1
+
+print(ans%(10**9+7))
---
+++ atcoder/abc178/e.py
@@ -0,0 +1,12 @@
+N = int(input())
+pulls = []
+minus = []
+
+for _ in range(N):
+    x, y = map(int, input().split())
+    pulls += [x+y]
+    minus += [x-y]
+
+pulls.sort()
+minus.sort()
+print(max(pulls[-1] - pulls[0], minus[-1] - minus[0]))
