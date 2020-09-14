bf1e46ec9d3544b9928997485c6057ab619c9ba3
Yuki Kimura <wakabame89@gmail.com>
2020-09-14 20:07:24 +0900

    caluculate combination

---
+++ comb.py
@@ -0,0 +1,10 @@
+def perm(n, k, p):
+  ret = 1
+  for i in range(n, n-k, -1):
+    ret = (ret * i)%p
+  return ret
+
+def comb(n, k, p):
+  a = perm(n, k, p)
+  b = perm(k, k, p)
+  return (a*pow(b, -1, p))%p
