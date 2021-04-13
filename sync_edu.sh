771eafc246c3b247818351c51d04401cfb17007d
Yuki Kimura <wakabame89@gmail.com>
2021-04-13 15:31:47 +0900

    add sync_edu

---
+++ sync_edu.sh
@@ -0,0 +1,9 @@
+cd kyopuro_edu_90/
+git fetch upstream
+git merge upstream/main
+git commit -m "merge main"
+git push origin main
+cd ..
+git add kyopuro_edu_90
+git commit -m "update"
+git push origin master
