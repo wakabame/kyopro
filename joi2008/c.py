1eaca0e0e21a5340c603f4d24fdcfd63c1cf2f89
Yuki Kimura <wakabame89@gmail.com>
2021-04-22 15:29:02 +0900

    update

---
+++ joi2008/c.py
@@ -0,0 +1,16 @@
+import bisect
+N, M = map(int, input().split())
+P = [0] + [int(input()) for _ in range(N)]
+
+Q = sorted([p+q for p in P for q in P])
+
+ans = 0
+for q in Q:
+    rem = M - q
+    index = bisect.bisect(Q, rem)
+    p = Q[index - 1]
+    if p + q > M:
+        continue
+    ans = max(ans, p+q)
+
+print(ans)
