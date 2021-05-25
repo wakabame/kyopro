0583a821d7bfb038b01e5a3409c604d2c392274a
Yuki Kimura <wakabame89@gmail.com>
2021-05-25 10:08:43 +0900

    add heuristics

---
+++ atcoder_marathon/future-contest-2020-qual/answer.py
@@ -0,0 +1,86 @@
+N = 100
+X, Y = [], []
+table = [[0 for i in range(N)] for j in range(N)]
+for i in range(N):
+    x, y = map(int, input().split())
+    X += [x]
+    Y += [y]
+    table[x][y] = i
+
+curr_x = 0
+curr_y = 0
+ans_str = ""
+
+def move_x(curr_x, dx, ans_str):
+    curr_x += dx
+    if dx > 0:
+        ans_str += "D" * dx
+    else:
+        ans_str += "U" * (-dx)
+
+    return curr_x, ans_str
+
+def move_y(curr_y, dy, ans_str):
+    curr_y += dy
+    if dy > 0:
+        ans_str += "R" * dy
+    else:
+        ans_str += "L" * (-dy)
+
+    return curr_y, ans_str
+    
+def move_and_append(curr_x, curr_y, i, ans_str):
+    x, y = X[i], Y[i]
+
+    if (i < N-1 and curr_y == Y[i+1]) and (curr_x-X[i+1])*(X[i+1]- x) > 0:
+        dx = X[i+1] - curr_x
+        curr_x, ans_str = move_x(curr_x, dx, ans_str)
+        ans_str += "I"
+        # TODO: 最後の空き地まで移動しておろす
+        """
+        table[curr_x][curr_y] = 0
+        if dx > 0:# 下向き
+            for x_ in range(X[i], X[i+1]+1):
+                if table[x_][curr_y] == 0:
+                    dx = x_ - curr_x
+        curr_x, ans_str = move_x(curr_x, dx, ans_str)
+        """
+        ans_str += "O"
+        # TODO: X[i+1] を書き換える
+        table[curr_x][curr_y] = i+1
+        X[i+1] = curr_x
+
+        dx = X[i] - curr_x
+        curr_x, ans_str = move_x(curr_x, dx, ans_str)
+    else:
+        dx = x - curr_x
+        curr_x, ans_str = move_x(curr_x, dx, ans_str)
+    
+    if (i < N-1 and curr_x == X[i+1]) and (curr_y-Y[i+1])*(Y[i+1]- y) > 0:
+        dy = Y[i+1] - curr_y
+        curr_y, ans_str = move_y(curr_y, dy, ans_str)
+        ans_str += "I"
+        # TODO: 最後の空き地まで移動する
+        ans_str += "O"
+        # TODO: X[i+1] を書き換える
+        dy = Y[i] - curr_y
+        curr_y, ans_str = move_y(curr_y, dy, ans_str)
+    else:
+        dy = y - curr_y
+        
+        curr_y, ans_str = move_y(curr_y, dy, ans_str)
+
+    return curr_x, curr_y, ans_str
+
+for i in range(N):
+    curr_x, curr_y, ans_str = move_and_append(curr_x, curr_y, i, ans_str)
+
+    ans_str += "I"
+
+
+print(ans_str)
+# U .. x--
+# D .. x++
+# L .. y--
+# R .. y++
+# DDDDDDDDDDDDDDDRRRRRRRRRRRRRRRRRRRIUUUUUUULLLLLLLLLLLLLLLLLLIUUUUURRRRRRRRRRRRIURRRRRRIDDDDDDDDDDDDDDDLLLLLLLLLIUUULLLLLLLIUUUUUUUUUUULIDDDIDDDODDDDDDDDDDRRIUUUUUUUUUULLIDDDDDDDDDLIUUUUUUUUUUUUUURRRIURRRRRRIUUURRRRRIDDLLLLLLLLLLIDDDDDDDDRRIUUUULLLLIDDDDDDDDDDDDDRRRRRRRRRIUUUUUUUUUUUUUUUUUULLLLLLLLLLLLIDDDDDDDDDDDDDDDDDDRRRIUUUUUUUUUUUUUUULLIUUUURRRRRIDDDDDDDDDDRRRRRRRRRRRRIDDLLLLLLIUUUURIUULLLLLLLLLIDDDDLLIUUUURRRRRRRRRRIUUUULLLLLLLLLLLLIUURRRRRRRRRRRIDDDDDDLLIDDLLLLLLIDDDDDRRRRRRIUUUULLLLLLLLLIDDRRRRRRRRRRRRRRRRRIUULLLLLLLLLLLLLIDDDLLLIUUUUUUUUUUURRRRRRRRRRRRRRRRRIDDDDDDDDDDDDDDDDDDRIUUUUUUUUUULLLLLLLLLLIUUUUUUURRIDDDDDDRRRRRRRRIDDDDDDDDDDLLLLLLLLLLLLLLLLIUUUUUUUUUUUUUUUURRRRRRRRRRRRIDDDDDDRIDDDDDDDDLLLLLLLLLLLLLLIUUUUUUUUUUUURRRIDDDDDDDDDDLIUUUUULIDDDDDDRRRRRRRRRRIUUUUUUUUUUUULLLLLLLLLLLLLIDDDDDRRRRRRRRRRRIDDDDDDDLLLLLLLIUUUUUUUUUUUUUUURRRIDDDDDDDDDDDDRRRRRRRRRRRRIDDDDDDLLLLLLLLLLLLIUUUUUURRRRRRRRRRIUUUULLLLLLLLLLLLLLLIUULLIUUUUURRRRRRRIDDDDDDDDDDDDDDDDLLLLLLLIURRRRRRRRRRIUUUUUUUUUUUUUUULLLLIDDDDDDDDDLIUUUUUURRRRRRRRRIDDDDDDDDLLLLLLLIUUUUUURRRRILLLLLIDDDDDDDDRRRRRRRRRRRRRIDLLLLIDDRIUUUUUUUUUUUUUUUULLLLIDDDDDDLLLIUUUUULLIDDIUUULLLIDDDDLIUUUUURRRRRRRRRRRRRRIDDDDDDDDDDDDDDDDDLLLLLLLLLLLLLLIUUUUUUUUUUUURRRRRRRRRRIDDDDRRRIDDDLLLIULIDDDDRRRRRRIUUUUUUUUUUUUUUULLLLLLLLIDDDDDDDDDDDDDDDDDDDLLLLIUUUUUUULLIUURRRRIDDDDDLLLLLLLIUUUUURRRRRRRRRRRRIDDDDDDDRRIUUUUUUUUUUUUUUUULLLLLLLLLLIDDDDDDDDDDDDDDRRRRRRRRRIDDDDLLLLLLLLLIUURRIUUUUUUUUUUUUIUUUUUUUUUUUUODDDDDDDDDRRRRRRRRRRRIUUUUUUUUULLLLLLLLLLLIDDDDDDDDDDDDDDDDDDDLIUUUUUUUUUIDDDDDDDDDDDDDDDDRIUUUUUUUUUUUUUUUUUURRRRRRRRRI
---
+++ atcoder_marathon/intro-heuristics/2_score_diff.cpp
@@ -0,0 +1,49 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+const static int NUM_TYPE = 26;
+
+int getFullScore(int D, const vector<int>& C, const vector<int>& T, const vector<vector<int>>& S) {
+    int ret = 0;
+    vector<int> last(NUM_TYPE, -1);
+    for (int d = 0; d < D; d++) {
+        const int j = T[d];
+        last[j] = d ;
+        for (int i = 0; i < NUM_TYPE; i++) {
+            ret -= (d - last[i]) * C[i];
+        }
+        ret += S[d][j];
+ 
+    // B 問題用の出力
+    cout << ret << endl;
+    }
+    return ret;
+}
+
+
+
+int main(){
+    int D;
+    cin >> D;
+    vector<int> C(NUM_TYPE);
+    for (int &c : C) {
+        cin >> c;
+    }
+    vector<vector<int>> S(D, vector<int>(NUM_TYPE));
+    for (auto &s : S) {
+        for (auto &x : s) {
+            cin >> x;
+        }
+    }
+    vector<int> T(D, -1);
+
+    // B 問題用の初期解
+    for (auto &t :T) {
+        cin >> t;
+        t--;
+    }
+
+    int fullscore = getFullScore(D, C, T, S);
+    
+    return 0;
+}
